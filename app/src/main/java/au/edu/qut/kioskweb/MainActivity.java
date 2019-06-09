d67f8265e8ae53233b4d42d27fbd4129efb9f0e9
benoit.favre <benoit.favre@qut.edu.au>
2019-06-10 09:54:28 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/qut/kioskweb/MainActivity.java
@@ -0,0 +1,178 @@
+package au.edu.qut.kioskweb;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import au.edu.qut.kiosk.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private final String TAG = "KioskWeb";
+    private final List blockedKeys = new ArrayList(); //new ArrayList(Arrays.asList(KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_VOLUME_UP));
+
+    protected void showWebView(final String startUrl) {
+        setContentView(R.layout.activity_main);
+        WebView web = (WebView) findViewById(R.id.web);
+
+        web.loadUrl(startUrl);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        setupApp();
+    }
+
+    private void setupApp() {
+        final SharedPreferences preferences = this.getPreferences(Context.MODE_PRIVATE);
+        String startUrl = preferences.getString(getString(R.string.start_url_key), getResources().getString(R.string.default_start_url));
+
+        if (isMyAppLauncherDefault()) {
+            showWebView(startUrl);
+        } else {
+            setContentView(R.layout.configuration);
+
+            final EditText startUrlEdit = (EditText) findViewById(R.id.start_url_config);
+            startUrlEdit.setText(startUrl);
+            startUrlEdit.addTextChangedListener(new TextWatcher() {
+
+                public void afterTextChanged(Editable s) {
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString(getString(R.string.start_url_key), s.toString());
+                    editor.commit();
+                }
+
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+            });
+
+
+            final Button testButton = (Button) findViewById(R.id.test_config);
+            testButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Toast.makeText(getApplicationContext(), "Loading web view", Toast.LENGTH_LONG).show();
+                    showWebView(startUrlEdit.getText().toString());
+                }
+            });
+
+        }
+
+        // every time someone enters the kiosk mode, set the flag true
+        PrefUtils.setKioskModeActive(true, getApplicationContext());
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        Log.d(TAG, "window focus changed " + hasFocus);
+        if (!hasFocus) {
+            // Close every kind of system dialog
+            /*Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+            sendBroadcast(closeDialog);*/
+            //if(recording) stopRecording();
+
+        }
+        if (hasFocus) {
+            hideSystemUI();
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // nothing to do here
+        // … really
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        Log.d(TAG, "dispatch key event " + event.getKeyCode());
+        if (blockedKeys.contains(event.getKeyCode())) {
+            return true;
+        } else {
+            return super.dispatchKeyEvent(event);
+        }
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Shows the system bars by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    // check if we are the launcher app
+// https://stackoverflow.com/questions/27991656/how-to-set-default-app-launcher-programmatically
+    private boolean isMyAppLauncherDefault() {
+        final IntentFilter filter = new IntentFilter(Intent.ACTION_MAIN);
+        filter.addCategory(Intent.CATEGORY_HOME);
+
+        List<IntentFilter> filters = new ArrayList<IntentFilter>();
+        filters.add(filter);
+
+        final String myPackageName = getPackageName();
+        List<ComponentName> activities = new ArrayList<ComponentName>();
+        final PackageManager packageManager = getPackageManager();
+
+        packageManager.getPreferredActivities(filters, activities, null);
+
+        for (ComponentName activity : activities) {
+            if (myPackageName.equals(activity.getPackageName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
